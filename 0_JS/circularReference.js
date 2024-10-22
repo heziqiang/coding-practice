4a0d07cbf6e34747c2a2076898957f26f28a3509
Ziqiang He <ziqianghe123@gmail.com>
2024-10-22 16:09:20 +0800

    feat: circle reference

--- 0_JS/circularReference.js
+++ 0_JS/circularReference.js
@@ -1,31 +1,35 @@
-// 判断对象是否存在循环引用
+// Determine if an object has a circular reference
 
-/*
-解法一 利用 JSON.stringify()对该类对象进行序列化，存在循环引用就会报错: Converting circular structure to JSON.
-*/
+// Method 1: use JSON.stringify, throw error if circular reference
 function hasCycle(obj) {
   try {
     JSON.stringify(obj);
   } catch (e) {
-    if (e.message.includes("circular")) {
+    if (e.message.includes('circular')) {
       return true;
     }
   }
   return false;
 }
 
-/*
-解法二 深度遍历对象，判断是否存在属性 等于 祖先对象
-@param obj: object 判断对象
-@param parent: object[] 所有父级对象
-*/
-function hasCycle2(obj, parent = []) {
-  return Object.values(obj).some((val) => {
-    if (parent.includes(val)) return true;
-    if (typeof val === "object") {
-      return hasCycle2(val, [obj, ...parent]);
+// Method 2: use a Set to store visited objects
+// Time complexity: O(n)
+// Space complexity: O(n)
+function hasCycle2(obj) {
+  const visited = new Set();
+
+  function detect(obj) {
+    if (obj && typeof obj === 'object') {
+      if (visited.has(obj)) return true;
+      visited.add(obj);
+      for (let val of Object.values(obj)) {
+        if (detect(val)) return true;
+      }
     }
-  });
+    return false;
+  }
+
+  return detect(obj);
 }
 
 // Test
