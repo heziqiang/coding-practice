bb031b1a8b8a6e703be77c569166bb47763e284a
Ziqiang He <ziqianghe123@gmail.com>
2024-11-12 11:52:52 +0800

    refactor: rename cur

--- leetcode_problems/2. Add Two Numbers.js
+++ leetcode_problems/2. Add Two Numbers.js
@@ -27,13 +27,13 @@ function addTwoNumbers(l1, l2) {
   let p2 = l2;
   let carry = 0;
   const dummy = new ListNode(0);
-  let tail = dummy;
+  let cur = dummy;
   while (p1 || p2 || carry > 0) {
     const sum = (p1 ? p1.val : 0) + (p2 ? p2.val : 0) + carry;
     const digit = sum % 10;
     carry = Math.floor(sum / 10);
-    tail.next = new ListNode(digit, null);
-    tail = tail.next;
+    cur.next = new ListNode(digit, null);
+    cur = cur.next;
 
     if (p1) p1 = p1.next;
     if (p2) p2 = p2.next;
--- leetcode_problems/21. Merge Two Sorted Lists.js
+++ leetcode_problems/21. Merge Two Sorted Lists.js
@@ -24,25 +24,25 @@ import { ListNode } from './utils.js';
 
 function mergeTwoLists(list1, list2) {
   const dummy = new ListNode(0);
-  let tail = dummy;
+  let cur = dummy;
   let p1 = list1;
   let p2 = list2;
   while (p1 || p2) {
     if (!p1) {
-      tail.next = p2;
+      cur.next = p2;
       break;
     }
     if (!p2) {
-      tail.next = p1;
+      cur.next = p1;
       break;
     }
     if (p1.val < p2.val) {
-      tail.next = p1;
-      tail = tail.next;
+      cur.next = p1;
+      cur = cur.next;
       p1 = p1.next;
     } else {
-      tail.next = p2;
-      tail = tail.next;
+      cur.next = p2;
+      cur = cur.next;
       p2 = p2.next;
     }
   }
