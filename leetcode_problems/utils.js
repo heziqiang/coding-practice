ae49181d2b6f46d96dd3914aec1ad79c71177ea1
Ziqiang He <ziqianghe123@gmail.com>
2024-12-01 15:44:58 +0800

    feat: utils add tree constructor

--- leetcode_problems/utils.js
+++ leetcode_problems/utils.js
@@ -26,3 +26,30 @@ export function printList(head) {
   }
   console.log(res.join('-'));
 }
+
+export function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
+
+export function createTree(arr) {
+  if (!arr.length) return null;
+  let root = new TreeNode(arr[0]);
+  let queue = [root];
+  arr.shift();
+  while (arr.length) {
+    let node = queue.shift();
+    let val = arr.shift();
+    if (val != null) {
+      node.left = new TreeNode(val);
+      queue.push(node.left);
+    }
+    val = arr.shift();
+    if (val != null) {
+      node.right = new TreeNode(val);
+      queue.push(node.right);
+    }
+  }
+  return root;
+}
