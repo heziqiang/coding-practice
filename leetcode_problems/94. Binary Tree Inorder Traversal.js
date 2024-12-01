a986ff3d5968af7c0eaeb05afc874d4b6fe5d3eb
Ziqiang He <ziqianghe123@gmail.com>
2024-12-01 17:19:05 +0800

    feat: binary tree inorder traversal

---
+++ leetcode_problems/94. Binary Tree Inorder Traversal.js
@@ -0,0 +1,32 @@
+/*
+94. Binary Tree Inorder Traversal
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+Example:
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+Output: [4,2,6,5,7,1,3,9,8]
+*/
+
+/*
+Approach: Use DFS recursive traversal
+
+Time complexity: O(n)
+Space complexity: O(log(n)) for recursion stack
+*/
+
+function inorderTraversal(root) {
+  const res = [];
+  function dfs(node) {
+    if (!node) return;
+    dfs(node.left);
+    res.push(node.val);
+    dfs(node.right);
+  }
+  dfs(root);
+  return res;
+}
+
+// Test
+import { createTree } from './utils.js';
+const tree = createTree([1, 2, 3, 4, 5, null, 8, null, null, 6, 7, 9]);
+console.log(inorderTraversal(tree)); // [4,2,6,5,7,1,3,9,8]
