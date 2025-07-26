f1a0c30a5fcf3b23f6114b48c0ee46cc3b786d56
Ziqiang <ziqianghe123@gmail.com>
2025-07-26 19:43:36 +0800

    feat: update problem 3

--- leetcode_problems/3. Longest Substring Without Repeating Characters.js
+++ leetcode_problems/3. Longest Substring Without Repeating Characters.js
@@ -6,28 +6,35 @@ Example:
 Input: s = "pwwkew"
 Output: 3
 Explanation: The answer is "wke", with the length of 3.
+
+Constraints:
+0 <= s.length <= 5 * 104
+s consists of English letters, digits, symbols and spaces.
 */
 
 /*
-Approach: Use sliding window, use two pointers left and right to represent the window.
-Move right if there is no duplicate character, move left if there is a dumplicate character.
+Approach:
+Use a sliding window, two pointers left and right to track the window boundaries.
+Use a set to store characters in the window, maxLen to track max window width ever.
+Move right to expand the window; if a duplicate character appears, move left to shrink the window until it is unique.
+Finally, return maxLen.
 
 Time complexity: O(n)
-Space complexity: O(n)
+Space complexity: O(n) for the set size
 */
 
 function lengthOfLongestSubstring(s) {
   let left = 0;
   let right = 0;
   let maxLen = 0;
-  const map = new Map();
+  const set = new Set();
   while (right < s.length) {
     const char = s[right];
-    while (map.has(char)) {
-      map.delete(s[left]);
+    while (set.has(char)) {
+      set.delete(s[left]);
       left++;
     }
-    map.set(char, 1);
+    set.add(char);
     maxLen = Math.max(maxLen, right - left + 1);
     right++;
   }
@@ -35,6 +42,6 @@ function lengthOfLongestSubstring(s) {
 }
 
 // Test
-console.log(lengthOfLongestSubstring('pwwkew')); // 3
-console.log(lengthOfLongestSubstring('bbbbb')); // 1
-console.log(lengthOfLongestSubstring('')); // 0
+console.log(lengthOfLongestSubstring("pwwkew")); // 3
+console.log(lengthOfLongestSubstring("bbbbb")); // 1
+console.log(lengthOfLongestSubstring("")); // 0
