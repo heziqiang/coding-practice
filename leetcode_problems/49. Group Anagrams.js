e6dddde64669bf378388aff901945ebb00d03424
Ziqiang <ziqianghe123@gmail.com>
2025-07-27 23:10:31 +0800

    feat: update problem 49

--- leetcode_problems/49. Group Anagrams.js
+++ leetcode_problems/49. Group Anagrams.js
@@ -2,27 +2,30 @@
 49. Group Anagrams
 Given an array of strings strs, group the anagrams together. You can return the answer in any order.
 
-Example 1:
+Example:
 Input: strs = ["eat","tea","tan","ate","nat","bat"]
 Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
-Explanation:
-There is no string in strs that can be rearranged to form "bat".
-The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
-The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+
+Constraints:
+1 <= strs.length <= 104
+0 <= strs[i].length <= 100
+strs[i] consists of lowercase English letters.
 */
 
 /*
-Approach: Use a Map to store sorted string as key, and the list of anagrams as value.
+Approach:
+Use a map where the key is sorted string, and the value is the list of its anagrams.
+Iterate through strs, for each string, sort it and add it to the map.
+Finally, return the map values.
 
-Time complexity: O(n * k * log(k)), n is the length of strs, k is the max length of string in strs.
-  quick sort takes O(k * log(k)) time.
-Space complexity: O(n * k)s
+Time complexity: O(n * k * log(k)), n is the length of strs, and k is the maximum string length, quick sort takes O(k * log(k)).
+Space complexity: O(n * k) for map size
 */
 
 function groupAnagrams(strs) {
   const map = new Map();
   for (let str of strs) {
-    const sorted = str.split('').sort().join('');
+    const sorted = str.split("").sort().join("");
     if (!map.has(sorted)) {
       map.set(sorted, []);
     }
@@ -32,5 +35,5 @@ function groupAnagrams(strs) {
 }
 
 // Test
-console.log(groupAnagrams(['eat', 'tea', 'tan', 'ate', 'nat', 'bat'])); // [["bat"],["nat","tan"],["ate","eat","tea"]]
-console.log(groupAnagrams([''])); // [['']]
+console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])); // [["bat"],["nat","tan"],["ate","eat","tea"]]
+console.log(groupAnagrams([""])); // [['']]
