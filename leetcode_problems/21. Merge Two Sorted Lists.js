86ee17ad5171e2d83ddfc70381a0caed198ff68c
Ziqiang <ziqianghe123@gmail.com>
2025-07-28 18:40:17 +0800

    feat: update problem 21

--- leetcode_problems/21. Merge Two Sorted Lists.js
+++ leetcode_problems/21. Merge Two Sorted Lists.js
@@ -16,34 +16,37 @@ Both list1 and list2 are sorted in non-decreasing order.
 */
 
 /*
-Approach: use Two Pointers traversal through the two lists,
-Compare the values of the two nodes, pick the smaller one and move on.
-If one list is empty, append the rest of the other list.
+Approach:
+Use two pointers, p1 and p2, to iterate through list1 and list2,
+Compare their values, append the smaller one to the result, then move on that pointer.
+When one list ends, append the remaining elements of the other list.
 
 Time complexity: O(n)
 Space complexity: O(1)
 */
-import { ListNode } from './utils.js';
+import { ListNode } from "./utils.js";
 
 function mergeTwoLists(list1, list2) {
   const dummy = new ListNode();
   let cur = dummy;
-  while (list1 && list2) {
-    if (list1.val < list2.val) {
-      cur.next = list1;
-      list1 = list1.next;
+  let p1 = list1;
+  let p2 = list2;
+  while (p1 && p2) {
+    if (p1.val < p2.val) {
+      cur.next = p1;
+      p1 = p1.next;
     } else {
-      cur.next = list2;
-      list2 = list2.next;
+      cur.next = p2;
+      p2 = p2.next;
     }
     cur = cur.next;
   }
-  cur.next = list1 ? list1 : list2;
+  cur.next = p1 ? p1 : p2;
   return dummy.next;
 }
 
 // Test
-import { createList, printList } from './utils.js';
-const list1 = createList([1, 2, 4]);
-const list2 = createList([1, 3]);
-printList(mergeTwoLists(list1, list2)); // 1-1-2-3-4
+import { createList, printList } from "./utils.js";
+
+printList(mergeTwoLists(createList([1, 2, 4, 5]), createList([1, 3]))); // 1-1-2-3-4-5
+printList(mergeTwoLists(createList([1, 2]), null)); // 1-2
