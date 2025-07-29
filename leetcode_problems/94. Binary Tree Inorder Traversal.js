61b30c1eaa0259561c05f4139f8de585c87bb816
Ziqiang <ziqianghe123@gmail.com>
2025-07-29 21:02:23 +0800

    feat: update problem 94

--- leetcode_problems/94. Binary Tree Inorder Traversal.js
+++ leetcode_problems/94. Binary Tree Inorder Traversal.js
@@ -5,6 +5,10 @@ Given the root of a binary tree, return the inorder traversal of its nodes' valu
 Example:
 Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
 Output: [4,2,6,5,7,1,3,9,8]
+
+Constraints:
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
 */
 
 /*
@@ -27,6 +31,6 @@ function inorderTraversal(root) {
 }
 
 // Test
-import { createTree } from './utils.js';
+import { createTree } from "./utils.js";
 const tree = createTree([1, 2, 3, 4, 5, null, 8, null, null, 6, 7, 9]);
 console.log(inorderTraversal(tree)); // [4,2,6,5,7,1,3,9,8]
