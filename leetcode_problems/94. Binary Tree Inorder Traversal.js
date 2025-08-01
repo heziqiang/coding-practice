925e4a62816337d1a5853f5a261961bf7cda1859
Ziqiang <ziqianghe123@gmail.com>
2025-08-01 16:53:37 +0800

    feat: update problem 94 iterative approch

--- leetcode_problems/94. Binary Tree Inorder Traversal.js
+++ leetcode_problems/94. Binary Tree Inorder Traversal.js
@@ -12,25 +12,27 @@ The number of nodes in the tree is in the range [0, 100].
 */
 
 /*
-Approach: Use DFS recursive traversal
-
-Time complexity: O(n)
-Space complexity: O(log(n)) for recursion stack
+Approach:
+DFS iterative traversal
+refer to { dfsIterative } from "../algorithm/dfs.js"
 */
-
 function inorderTraversal(root) {
-  const res = [];
-  function dfs(node) {
-    if (!node) return;
-    dfs(node.left);
-    res.push(node.val);
-    dfs(node.right);
+  const result = [];
+  const stack = [[root, false]];
+  while (stack.length) {
+    const [node, visited] = stack.pop();
+    if (visited) {
+      result.push(node.val);
+    } else {
+      if (node.right) stack.push([node.right, false]);
+      stack.push([node, true]);
+      if (node.left) stack.push([node.left, false]);
+    }
   }
-  dfs(root);
-  return res;
+  return result;
 }
 
 // Test
 import { createTree } from "./utils.js";
 const tree = createTree([1, 2, 3, 4, 5, null, 8, null, null, 6, 7, 9]);
-console.log(inorderTraversal(tree)); // [4,2,6,5,7,1,3,9,8]
+console.log(inorderTraversal(tree)); // [4, 2, 6, 5, 7, 1, 3, 9, 8]
