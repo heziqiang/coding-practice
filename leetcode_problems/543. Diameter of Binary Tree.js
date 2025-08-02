6659f416d7513744f46226f1136c59d7b4e07feb
Ziqiang <ziqianghe123@gmail.com>
2025-08-02 10:14:25 +0800

    feat: update problem 543

--- leetcode_problems/543. Diameter of Binary Tree.js
+++ leetcode_problems/543. Diameter of Binary Tree.js
@@ -9,50 +9,44 @@ Example:
 Input: root = [1,2,3,4,5]
 Output: 3
 Explanation: 3 is the length of the path [4,2,1,3] or [5,2,1,3].
+
+Constraints:
+The number of nodes in the tree is in the range [1, 104].
+-100 <= Node.val <= 100
 */
 
 /*
-Approach: The length of the longest path which pass a node, is sum of max depth of its left and right subtree.
-Traverse the tree for maxDepth of each node, store the max length.
+Approach:
+The length of the longest path which pass a node, is the sum of its left and right subtrees' max depth.
+Traverse the tree recursively, for each node, get the maxDepths of its children and update the max length.
+
+refer to problem: 104. Maximum Depth of Binary Tree
 
 Time complexity: O(n)
-Space complexity: O(log n) for recursion stack
+Space complexity: O(log n) - O(n) for recursion stack depth
 */
 
 function diameterOfBinaryTree(root) {
-  let max = 0;
-  function maxDepth(root) {
-    if (!root) return 0;
-    const left = maxDepth(root.left);
-    const right = maxDepth(root.right);
-    // update the max length
-    max = Math.max(max, left + right);
-    // get the max depth of node
+  let maxLen = 0;
+  function maxDepth(node) {
+    if (!node) return 0;
+    const left = maxDepth(node.left);
+    const right = maxDepth(node.right);
+    // longest path which pass the node
+    maxLen = Math.max(maxLen, left + right);
+    // max depth of the node
     return Math.max(left, right) + 1;
   }
   maxDepth(root);
-  return max;
+  return maxLen;
 }
 
 // Test
-import { createTree } from './utils.js';
-const tree = createTree([1, 2, 3, 4, 5]);
-const tree2 = createTree([
-  1,
-  2,
-  3,
-  4,
-  5,
-  null,
-  null,
-  6,
-  null,
-  null,
-  7,
-  8,
-  null,
-  null,
-  9,
-]);
-console.log(diameterOfBinaryTree(tree)); // 3
-console.log(diameterOfBinaryTree(tree2)); // 6, not pass the root
+import { createTree } from "./utils.js";
+
+console.log(diameterOfBinaryTree(createTree([1, 2, 3, 4, 5]))); // 3
+console.log(
+  diameterOfBinaryTree(
+    createTree([1, 2, 3, 4, 5, null, null, 6, null, null, 7, 8, null, null, 9])
+  )
+); // 6, will not pass the root
