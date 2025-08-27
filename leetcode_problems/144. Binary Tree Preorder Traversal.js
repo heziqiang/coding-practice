8976779f7d050a2f6ec87fbd85bf2cbeafdc0c40
Ziqiang <ziqianghe123@gmail.com>
2025-08-27 22:45:17 +0800

    feat: add problem 144

---
+++ leetcode_problems/144. Binary Tree Preorder Traversal.js
@@ -0,0 +1,12 @@
+/*
+144. Binary Tree Preorder Traversal
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+Example:
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+
+Constraints:
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+*/
